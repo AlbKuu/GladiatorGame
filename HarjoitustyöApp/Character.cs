7b4d8d81d257a5e10526d3ec2a9eb0914a70a26d
Albert Kuusjärvi <albert.kuusjarvi@gmail.com>
2016-03-08 19:22:49 +0200

    Initial commit

---
+++ HarjoitustyöApp/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjoitustyöApp
+{
+    class Character
+    {
+        public int Level;
+        public int HitPoints { get; set; }
+        public int Block;
+        public int Stamina;
+    }
+}
---
+++ HarjoitustyöApp/GamePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HarjoitustyöApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GamePage : Page
+    {
+        public GamePage()
+        {
+            this.InitializeComponent();
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null) return;
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+            }
+        }
+
+        private void CreateCharacterButton_Click(object sender, RoutedEventArgs e)
+        {
+            LevelTextBlock.Text = "Level=1";
+            HitPointsBar.Value = 50;
+            BlockTextBlock.Text = "Block=2";
+            StaminaBar.Value = 25;
+        }
+    }
+}
